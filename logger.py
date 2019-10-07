2dc5a7b425e1acce3c2dcb33659f5d5ff33a149a
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-10-07 14:46:48 -0700

    Complete write_metadata() func & test

--- logger.py
+++ logger.py
@@ -1,21 +1,14 @@
 class Logger(object):
     ''' Utility class responsible for logging all interactions during the simulation. '''
-    # TODO: Write a test suite for this class to make sure each method is working
-    # as expected.
-
-    # PROTIP: Write your tests before you solve each function, that way you can
-    # test them one by one as you write your class.
 
     def __init__(self, file_name):
         # TODO:  Finish this initialization method. The file_name passed should be the
         # full file name of the file that the logs will be written to.
-        self.file_name = None
+        self.file_name = file_name
 
-    def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
-                       basic_repro_num):
+    def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate, basic_repro_num):
         '''
-        The simulation class should use this method immediately to log the specific
-        parameters of the simulation as the first line of the file.
+        The simulation class should use this method immediately to log the specific parameters of the simulation as the first line of the file.
         '''
         # TODO: Finish this method. This line of metadata should be tab-delimited
         # it should create the text file that we will store all logs in.
@@ -23,10 +16,15 @@ class Logger(object):
         # the 'a' mode to append a new log to the end, since 'w' overwrites the file.
         # NOTE: Make sure to end every line with a '/n' character to ensure that each
         # event logged ends up on a separate line!
-        pass
+        initial_params = f"{pop_size}\t{vacc_percentage}\t{virus_name}\t{mortality_rate}\t{basic_repro_num}\n"
 
-    def log_interaction(self, person, random_person, random_person_sick=None,
-                        random_person_vacc=None, did_infect=None):
+        f = open(self.file_name, 'w')
+        f.write(initial_params)
+        f.close()
+
+        return initial_params
+
+    def log_interaction(self, person, random_person, random_person_sick=None, random_person_vacc=None, did_infect=None):
         '''
         The Simulation object should use this method to log every interaction
         a sick person has during each time step.
@@ -73,3 +71,15 @@ class Logger(object):
         # new one begins.
         # NOTE: Here is an opportunity for a stretch challenge!
         pass
+
+# TODO: Write a test suite for this class to make sure each method is working as expected.
+# PROTIP: Write your tests before you solve each function, that way you can test them one by one as you write your class.
+
+def test_logger():
+
+    '''Tests logger functions'''
+    sim1 = Logger('log.txt')
+    with open('log.txt') as f:
+        log_output = f.readline()
+
+    assert sim1.write_metadata(100, 10, 'Dengue Fever', 5, 3) == log_output
